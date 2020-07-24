40bf723be2c8938fc41c435f1c49aa1614e362c5
raudor <rivaspaulino@outlook.com>
2020-07-23 16:57:21 -0800

    lockbox algorithm

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+Determine if the box can be opened
+'''
+def canUnlockAll(boxes):
+    
+    open = {0}
+    for index, box in enumerate(boxes):
+        for key in box:
+
+            if key != index:
+                open.add(key)
+
+    for index in range(len(boxes)):
+
+        if index not in open:
+
+            return False
+    return True
