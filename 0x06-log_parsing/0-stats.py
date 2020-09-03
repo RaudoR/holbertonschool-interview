ca182fc5618a770062bc9e2e90826e0fc84dfdd6
raudor <rivaspaulino@outlook.com>
2020-09-02 17:55:08 -0700

    stats

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,48 +1,31 @@
 #!/usr/bin/python3
-'''
-Write a script that reads stdin line by line and computes metrics:
-Input format:
-<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
-After every 10 lines and/or a keyboard interruption (CTRL + C),
-print these statistics from the beginning:
-    Total file size: File size: <total size>
-    where <total size> is the sum of all previous <file size>
-    (see input format above)
-    Number of lines by status code:
-        possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-        if a status code doesn’t appear, don’t print anything for this
-        status code
-        format: <status code>: <number>
-        status codes should be printed in ascending order
-'''
+"""
+script
+"""
 import sys
 
 
-def print_status(size, status):
-    '''
-    Print the status
-    '''
-    print('File size: {}'.format(size))
-    for key, value in sorted(status.items()):
-        if value:
-            print('{}: {}'.format(key, value))
+file_size = 0 
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                403: 0, 404: 0, 405: 0, 500: 0}
 
+def print_stats(file_size, status_codes):
+    """print logs"""
+    print('File size: {}'.format(file_size))
+    for key in sorted(status_codes):
+        if status_codes[key] > 0:
+            print('{}: {}'.format(key, status_codes[key]))
 
-if __name__ == '__main__':
-    size, count = 0, 0
-    status = {'200': 0, '301': 0, '400': 0, '401': 0,
-              '403': 0, '404': 0, '405': 0, '500': 0}
+if __name__ == "__main__":
     try:
-        for line in sys.stdin:
-            args = line.split()
-            if len(args) > 2:
-                if args[-2] in status:
-                    status[args[-2]] += 1
-                size += int(args[-1])
-            count += 1
-            if not count % 10:
-                print_status(size, status)
+        for linenum, line in enumerate(sys.stdin, 1):
+            fields = line.split(" ")
+            if len(fields) <= 6:
+                pass
+            file_size += int(fields.pop())
+            status_codes[int(fields.pop())] += 1
+            if linenum % 10 == 0:
+                print_stats(file_size, status_codes)
+        print_stats(file_size, status_codes)
     except KeyboardInterrupt:
-        pass
-    finally:
-        print_status(size, status)
+        print_stats(file_size, status_codes)
