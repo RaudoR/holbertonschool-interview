8fd82dc5a20bde34573e1a20e9a1db51b6d2aa18
raudor <rivaspaulino@outlook.com>
2020-09-02 17:37:28 -0700

    new algorithm

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,36 +1,48 @@
 #!/usr/bin/python3
-""" Write a script that reads stdin line by line and computes metrics """
+'''
+Write a script that reads stdin line by line and computes metrics:
+Input format:
+<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+After every 10 lines and/or a keyboard interruption (CTRL + C),
+print these statistics from the beginning:
+    Total file size: File size: <total size>
+    where <total size> is the sum of all previous <file size>
+    (see input format above)
+    Number of lines by status code:
+        possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+        if a status code doesn’t appear, don’t print anything for this
+        status code
+        format: <status code>: <number>
+        status codes should be printed in ascending order
+'''
+import sys
+
+
+def print_status(size, status):
+    '''
+    Print the status
+    '''
+    print('File size: {}'.format(size))
+    for key, value in sorted(status.items()):
+        if value:
+            print('{}: {}'.format(key, value))
+
 
 if __name__ == '__main__':
-    import sys
-
-    fileSize, counter = 0, 0
-
-    stats = {"200": 0, "301": 0, "400": 0, "401": 0,
-             "403": 0, "404": 0, "405": 0, "500": 0}
-
-    def report(stats, fileSize):
-        print("File size: {:d}".format(fileSize))
-        for i in sorted(stats.keys()):
-            if stats[i] != 0:
-                print("{}: {:d}".format(i, stats[i]))
-
+    size, count = 0, 0
+    status = {'200': 0, '301': 0, '400': 0, '401': 0,
+              '403': 0, '404': 0, '405': 0, '500': 0}
     try:
         for line in sys.stdin:
-            if counter != 0 and counter % 10 == 0:
-                report(stats, fileSize)
-            strline = line.split()
-            counter += 1
-            try:
-                fileSize += int(strline[-1])
-            except:
-                pass
-            try:
-                if strline[-2] in stats:
-                    stats[strline[-2]] += 1
-            except:
-                pass
-        report(stats, fileSize)
+            args = line.split()
+            if len(args) > 2:
+                if args[-2] in status:
+                    status[args[-2]] += 1
+                size += int(args[-1])
+            count += 1
+            if not count % 10:
+                print_status(size, status)
     except KeyboardInterrupt:
-        report(stats, fileSize)
-        raise
+        pass
+    finally:
+        print_status(size, status)
