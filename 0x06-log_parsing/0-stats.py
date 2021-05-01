00118e2b31ef3e6044a7fc109bc1be629142a456
raudor <rivaspaulino@outlook.com>
2021-04-30 19:40:32 -0700

    task 0

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,63 +1,44 @@
 #!/usr/bin/python3
-"""
-Read log data line-by-line from stdin and print statistics to stdout after
-every ten lines or upon receipt of a SIGINT
-"""
-# pylint: disable=invalid-name
+"""A script that reads stdin line by line and computes metrics"""
+
 import sys
 
-STATUS_CODES = {200, 301, 400, 401, 403, 404, 405, 500}
+
+def print_status(dict, size):
+    """Print the format"""
+    print("File size: {}".format(size))
+    for key in sorted(dict.keys()):
+        if dict[key] != 0:
+            print("{}: {}".format(key, dict[key]))
 
 
-class DefaultDict(dict):
-    """Defines a dict subclass to set default values for missing keys"""
+status_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+               '404': 0, '405': 0, '500': 0}
 
-    def __init__(self, default, *args, **kwargs):
-        """Initializes a dict using the provided constructor for defaults"""
-        super().__init__(*args, **kwargs)
-        self.default = default
+file_size = 0
+count = 0
 
-    @property
-    def default(self):
-        """Gets the callable object used to construct default values"""
-        return self.__default
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            print_status(status_dict, file_size)
 
-    @default.setter
-    def default(self, default):
-        """Sets the callable object used to construct default values"""
-        if not callable(default):
-            raise TypeError("'default' must be a callable object")
-        self.__default = default
+        el = line.split(" ")
+        count += 1
 
-    def __getitem__(self, key):
-        """Gets the value of a mapping, setting a default if none exists"""
-        if key not in self:
-            self.__setitem__(key, self.default())
-        return super().__getitem__(key)
+        try:
+            file_size += int(el[-1])
+        except:
+            pass
 
-
-def print_stats(file_size, status_codes):
-    """Print log metrics to stdout"""
-    print('File size: {}'.format(file_size))
-    for code in filter(status_codes.get, sorted(STATUS_CODES)):
-        print('{}: {}'.format(code, status_codes[code]))
+        try:
+            if el[-2] in status_dict.keys():
+                status_dict[el[-2]] += 1
+        except:
+            pass
+    print_status(status_dict, file_size)
 
 
-if __name__ == '__main__':
-
-    file_size = 0
-    status_codes = DefaultDict(int)
-    try:
-        for lineno, line in enumerate(sys.stdin, 1):
-            fields = line.strip().split()
-            try:
-                file_size += int(fields.pop())
-                status_codes[int(fields.pop())] += 1
-            except (IndexError, ValueError):
-                pass
-            if lineno % 10 == 0:
-                print_stats(file_size, status_codes)
-        print_stats(file_size, status_codes)
-    except KeyboardInterrupt:
-        print_stats(file_size, status_codes)
-        raise
+except KeyboardInterrupt:
+    print_status(status_dict, file_size)
+    raise
